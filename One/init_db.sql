146f0518d77f528c56a0bbc4f16b6ea1ce9c3ed4
Daniel Moore <dxm9604@g.rit.edu>
2020-09-10 15:54:19 -0400

    Write scripts to create tables

---
+++ One/init_db.sql
@@ -0,0 +1,45 @@
+BEGIN;
+
+CREATE TABLE IF NOT EXISTS title(
+    titleID INTEGER PRIMARY KEY ,
+    titleType VARCHAR(100) NOT NULL ,
+    primaryTitle VARCHAR(100) NOT NULL ,
+    originalTitle VARCHAR(100) NOT NULL ,
+    isAdult BOOLEAN NOT NULL ,
+    startYear INTEGER NOT NULL ,
+    endYear INTEGER NOT NULL ,
+    runtimeMinutes INTEGER NOT NULL ,
+    genres text array
+);
+
+CREATE TABLE IF NOT EXISTS episode(
+    titleID INTEGER NOT NULL ,
+    seriesTitleID INTEGER NOT NULL ,
+    seasonNumber INTEGER NOT NULL ,
+    episodeNumber INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS crew(
+    titleID INTEGER NOT NULL ,
+    crewID INTEGER NOT NULL PRIMARY KEY
+);
+
+CREATE TABLE IF NOT EXISTS directors(
+    crewID INTEGER NOT NULL ,
+    peopleID INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS writers(
+    crewID INTEGER NOT NULL ,
+    peopleID INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS writers(
+  peopleID INTEGER NOT NULL ,
+  primaryName VARCHAR(150) NOT NULL ,
+  birthYear INTEGER NOT NULL ,
+  deathYear INTEGER NOT NULL
+);
+
+
+COMMIT;
