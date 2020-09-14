e0b67fa5571d2344afca8852e95456590bc4e26e
Daniel Moore <dxm9604@g.rit.edu>
2020-09-13 23:02:31 -0400

    Fix up create table script

--- One/init_db.sql
+++ One/init_db.sql
@@ -12,46 +12,66 @@ CREATE TABLE IF NOT EXISTS title(
     genres text array
 );
 
+CREATE TABLE IF NOT EXISTS people(
+     peopleID INTEGER PRIMARY KEY ,
+     primaryName VARCHAR(150) NOT NULL ,
+     birthYear INTEGER NOT NULL ,
+     deathYear INTEGER NOT NULL
+);
+
+CREATE TABLE IF NOT EXISTS crew(
+   crewID INTEGER NOT NULL PRIMARY KEY ,
+   titleID INTEGER NOT NULL ,
+   FOREIGN KEY(titleID)
+       REFERENCES title(titleID)
+);
+
 CREATE TABLE IF NOT EXISTS episode(
     titleID INTEGER NOT NULL ,
     seriesTitleID INTEGER NOT NULL ,
     seasonNumber INTEGER NOT NULL ,
-    episodeNumber INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS crew(
-    titleID INTEGER NOT NULL ,
-    crewID INTEGER NOT NULL PRIMARY KEY
+    episodeNumber INTEGER NOT NULL ,
+    FOREIGN KEY(titleID)
+        REFERENCES title(titleID),
+    FOREIGN KEY(seriesTitleID)
+        REFERENCES crew(crewID)
 );
 
 CREATE TABLE IF NOT EXISTS directors(
     crewID INTEGER NOT NULL ,
-    peopleID INTEGER NOT NULL
+    peopleID INTEGER NOT NULL ,
+    FOREIGN KEY(crewID)
+        REFERENCES crew(crewID) ,
+    FOREIGN KEY(peopleID)
+        REFERENCES people(peopleID)
 );
 
 CREATE TABLE IF NOT EXISTS writers(
     crewID INTEGER NOT NULL ,
-    peopleID INTEGER NOT NULL
-);
-
-CREATE TABLE IF NOT EXISTS people(
-  peopleID INTEGER NOT NULL ,
-  primaryName VARCHAR(150) NOT NULL ,
-  birthYear INTEGER NOT NULL ,
-  deathYear INTEGER NOT NULL
+    peopleID INTEGER NOT NULL ,
+    FOREIGN KEY(crewID)
+        REFERENCES crew(crewID) ,
+    FOREIGN KEY(peopleID)
+        REFERENCES people(peopleID)
 );
 
 CREATE TABLE IF NOT EXISTS principal(
      titleID INTEGER NOT NULL ,
      ordering INTEGER NOT NULL ,
      peopleID INTEGER NOT NULL ,
-     category text NOT NULL
+     category text NOT NULL ,
+     FOREIGN KEY(titleID)
+         REFERENCES title(titleID) ,
+     FOREIGN KEY(peopleID)
+         REFERENCES people(peopleID)
 );
 
 CREATE TABLE IF NOT EXISTS ratings(
     titleID INTEGER NOT NULL ,
     averageRating DECIMAL NOT NULL ,
-    numVotes INTEGER NOT NULL
+    numVotes INTEGER NOT NULL ,
+    FOREIGN KEY(titleID)
+        REFERENCES title(titleID)
 );
 
 COMMIT;
