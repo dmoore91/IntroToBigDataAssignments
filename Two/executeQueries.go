e1f817a0ea39ae2eddddc889022248120ba4c2d1
dmoore91 <danmoore2706@gmail.com>
2020-09-23 23:42:23 -0400

    Add query for get actors whose name starts with Phi and didn't act in 2014

--- Two/executeQueries.go
+++ Two/executeQueries.go
@@ -39,7 +39,58 @@ func executeInvalidActorsQuery() {
 	}
 }
 
-func main() {
-	executeInvalidActorsQuery()
+func actorsNamedPhiAndDidntActIn2014() {
 
+	start := time.Now()
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "(SELECT name FROM Member " +
+		"INNER JOIN Title_Actor ON actor = Member.id " +
+		"WHERE deathYear IS NULL AND name LIKE 'Phi%' ) " +
+		"EXCEPT " +
+		"(SELECT name FROM Member " +
+		"INNER JOIN Title_Actor ON actor = Member.id INNER JOIN Title ON Title_Actor.title = Title.id " +
+		"WHERE deathYear IS NULL AND name LIKE 'Phi%' AND Title.startYear=2014)"
+
+	rows, err := conn.Query(context.Background(), queryString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	//End the timing here since printing has nothing to do
+	//with the speed of the query
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println("It took  " + elapsed.String() + " to run this query")
+
+	defer rows.Close()
+
+	fmt.Println("Names:")
+	for rows.Next() {
+
+		var name string
+
+		err = rows.Scan(&name)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		fmt.Println(name)
+
+	}
+
+	err = conn.Close(context.Background())
+	if err != nil {
+		log.Fatal(err)
+	}
+}
+
+func main() {
+	//executeInvalidActorsQuery()
+	actorsNamedPhiAndDidntActIn2014()
 }
