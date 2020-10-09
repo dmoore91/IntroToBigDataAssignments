148e3a5233c63e4119192a6455d13b4158095633
dmoore91 <danmoore2706@gmail.com>
2020-10-09 01:18:39 -0400

    Remove code that prevented you from having aaaa->b because that's a ridiculous thing to check. I think it it however against the assignment to optimize it but preventing duplication of columns so I removed that code

--- Three/naiveFunctionalDependencies.go
+++ Three/naiveFunctionalDependencies.go
@@ -325,7 +325,243 @@ func checkTwoOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 
 		for groupTwo := 0; groupTwo < 10; groupTwo++ {
 
-			if groupOne != groupTwo {
+			// All default to being valid functional dependencies. Change to false once we discover they are not
+			// Reset after each group
+			isValid := []bool{true, true, true, true, true, true, true, true, true, true}
+
+			// Need to create new maps for each group too or else we'll get collisions
+			maps := genericMapsNaive{
+				TitleID:   make(map[string]int),
+				TitleType: make(map[string]sql.NullString),
+				StartYear: make(map[string]sql.NullInt32),
+				Runtime:   make(map[string]int),
+				AvgRating: make(map[string]decimal.NullDecimal),
+				GenreId:   make(map[string]int),
+				Genre:     make(map[string]sql.NullString),
+				MemberId:  make(map[string]int),
+				BirthYear: make(map[string]sql.NullInt32),
+				Role:      make(map[string]string),
+			}
+
+			// Iterate through each row in db
+			for _, elem := range data {
+
+				key := getValueByColumnNum(groupOne, elem)
+				key += getValueByColumnNum(groupTwo, elem)
+
+				// titleID
+				titleID, ok := maps.TitleID[key]
+
+				if !ok {
+					maps.TitleID[key] = elem.TitleID
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.TitleID != titleID {
+
+						isValid[0] = false
+					}
+				}
+
+				// type
+				titleType, ok := maps.TitleType[key]
+
+				if !ok {
+					maps.TitleType[key] = elem.TitleType
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.TitleType.String != titleType.String {
+
+						isValid[1] = false
+					}
+				}
+
+				// startYear
+				startYear, ok := maps.StartYear[key]
+
+				if !ok {
+					maps.StartYear[key] = elem.StartYear
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.StartYear.Int32 != startYear.Int32 {
+						isValid[2] = false
+					}
+				}
+
+				// runtimeMinutes
+				runtimeMinutes, ok := maps.Runtime[key]
+
+				if !ok {
+					maps.Runtime[key] = elem.Runtime
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.Runtime != runtimeMinutes {
+						isValid[3] = false
+					}
+				}
+
+				// avgRating
+				avgRating, ok := maps.AvgRating[key]
+
+				if !ok {
+					maps.AvgRating[key] = elem.AvgRating
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if !elem.AvgRating.Decimal.Equal(avgRating.Decimal) {
+						isValid[4] = false
+					}
+				}
+
+				// genre_id
+				genreID, ok := maps.GenreId[key]
+
+				if !ok {
+					maps.GenreId[key] = elem.GenreId
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.GenreId != genreID {
+						isValid[5] = false
+					}
+				}
+
+				// genre
+				genre, ok := maps.Genre[key]
+
+				if !ok {
+					maps.Genre[key] = elem.Genre
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.Genre.String != genre.String {
+						isValid[6] = false
+					}
+				}
+
+				// member_id
+				memberID, ok := maps.MemberId[key]
+
+				if !ok {
+					maps.MemberId[key] = elem.MemberId
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.MemberId != memberID {
+						isValid[7] = false
+					}
+				}
+
+				// birthYear
+				birthYear, ok := maps.BirthYear[key]
+
+				if !ok {
+					maps.BirthYear[key] = elem.BirthYear
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.BirthYear.Int32 != birthYear.Int32 {
+						isValid[8] = false
+					}
+				}
+
+				// role
+				role, ok := maps.Role[key]
+
+				if !ok {
+					maps.Role[key] = elem.Role
+				} else {
+					// Since they differ, this is not a valid functional dependency
+					if elem.Role != role {
+						isValid[9] = false
+					}
+				}
+			}
+
+			header := ""
+
+			switch groupOne {
+			case 0:
+				header += "movieID,"
+			case 1:
+				header += "type,"
+			case 2:
+				header += "startYear,"
+			case 3:
+				header += "runtimeMinutes,"
+			case 4:
+				header += "avgRating,"
+			case 5:
+				header += "genre_id,"
+			case 6:
+				header += "genre,"
+			case 7:
+				header += "member_id,"
+			case 8:
+				header += "birthYear,"
+			case 9:
+				header += "role,"
+			}
+
+			switch groupTwo {
+			case 0:
+				header += "movieID->"
+			case 1:
+				header += "type->"
+			case 2:
+				header += "startYear->"
+			case 3:
+				header += "runtimeMinutes->"
+			case 4:
+				header += "avgRating->"
+			case 5:
+				header += "genre_id->"
+			case 6:
+				header += "genre->"
+			case 7:
+				header += "member_id->"
+			case 8:
+				header += "birthYear->"
+			case 9:
+				header += "role->"
+			}
+
+			for idx, valid := range isValid {
+				if valid && idx != groupOne && idx != groupTwo {
+					switch idx {
+					case 0:
+						println(header + "movieID")
+					case 1:
+						println(header + "type")
+					case 2:
+						println(header + "startYear")
+					case 3:
+						println(header + "runtimeMinutes")
+					case 4:
+						println(header + "avgRating")
+					case 5:
+						println(header + "genre_id")
+					case 6:
+						println(header + "genre")
+					case 7:
+						println(header + "member_id")
+					case 8:
+						println(header + "birthYear")
+					case 9:
+						println(header + "role")
+					}
+				}
+			}
+		}
+	}
+
+	fmt.Println("Check Two Done")
+}
+
+func checkThreeOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
+
+	defer wg.Done()
+
+	// Iterate through each column in db
+	for groupOne := 0; groupOne < 10; groupOne++ {
+
+		for groupTwo := 0; groupTwo < 10; groupTwo++ {
+
+			for groupThree := 0; groupThree < 10; groupThree++ {
 
 				// All default to being valid functional dependencies. Change to false once we discover they are not
 				// Reset after each group
@@ -350,6 +586,7 @@ func checkTwoOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 
 					key := getValueByColumnNum(groupOne, elem)
 					key += getValueByColumnNum(groupTwo, elem)
+					key += getValueByColumnNum(groupThree, elem)
 
 					// titleID
 					titleID, ok := maps.TitleID[key]
@@ -501,6 +738,29 @@ func checkTwoOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 
 				switch groupTwo {
 				case 0:
+					header += "movieID,"
+				case 1:
+					header += "type,"
+				case 2:
+					header += "startYear,"
+				case 3:
+					header += "runtimeMinutes,"
+				case 4:
+					header += "avgRating,"
+				case 5:
+					header += "genre_id,"
+				case 6:
+					header += "genre,"
+				case 7:
+					header += "member_id,"
+				case 8:
+					header += "birthYear,"
+				case 9:
+					header += "role,"
+				}
+
+				switch groupThree {
+				case 0:
 					header += "movieID->"
 				case 1:
 					header += "type->"
@@ -523,7 +783,7 @@ func checkTwoOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 				}
 
 				for idx, valid := range isValid {
-					if valid && idx != groupOne && idx != groupTwo {
+					if valid && idx != groupOne && idx != groupTwo && idx != groupThree {
 						switch idx {
 						case 0:
 							println(header + "movieID")
@@ -552,272 +812,6 @@ func checkTwoOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 		}
 	}
 
-	fmt.Println("Check Two Done")
-}
-
-func checkThreeOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
-
-	defer wg.Done()
-
-	// Iterate through each column in db
-	for groupOne := 0; groupOne < 10; groupOne++ {
-
-		for groupTwo := 0; groupTwo < 10; groupTwo++ {
-
-			for groupThree := 0; groupThree < 10; groupThree++ {
-
-				if groupOne != groupTwo && groupOne != groupThree && groupTwo != groupThree {
-
-					// All default to being valid functional dependencies. Change to false once we discover they are not
-					// Reset after each group
-					isValid := []bool{true, true, true, true, true, true, true, true, true, true}
-
-					// Need to create new maps for each group too or else we'll get collisions
-					maps := genericMapsNaive{
-						TitleID:   make(map[string]int),
-						TitleType: make(map[string]sql.NullString),
-						StartYear: make(map[string]sql.NullInt32),
-						Runtime:   make(map[string]int),
-						AvgRating: make(map[string]decimal.NullDecimal),
-						GenreId:   make(map[string]int),
-						Genre:     make(map[string]sql.NullString),
-						MemberId:  make(map[string]int),
-						BirthYear: make(map[string]sql.NullInt32),
-						Role:      make(map[string]string),
-					}
-
-					// Iterate through each row in db
-					for _, elem := range data {
-
-						key := getValueByColumnNum(groupOne, elem)
-						key += getValueByColumnNum(groupTwo, elem)
-						key += getValueByColumnNum(groupThree, elem)
-
-						// titleID
-						titleID, ok := maps.TitleID[key]
-
-						if !ok {
-							maps.TitleID[key] = elem.TitleID
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.TitleID != titleID {
-
-								isValid[0] = false
-							}
-						}
-
-						// type
-						titleType, ok := maps.TitleType[key]
-
-						if !ok {
-							maps.TitleType[key] = elem.TitleType
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.TitleType.String != titleType.String {
-
-								isValid[1] = false
-							}
-						}
-
-						// startYear
-						startYear, ok := maps.StartYear[key]
-
-						if !ok {
-							maps.StartYear[key] = elem.StartYear
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.StartYear.Int32 != startYear.Int32 {
-								isValid[2] = false
-							}
-						}
-
-						// runtimeMinutes
-						runtimeMinutes, ok := maps.Runtime[key]
-
-						if !ok {
-							maps.Runtime[key] = elem.Runtime
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.Runtime != runtimeMinutes {
-								isValid[3] = false
-							}
-						}
-
-						// avgRating
-						avgRating, ok := maps.AvgRating[key]
-
-						if !ok {
-							maps.AvgRating[key] = elem.AvgRating
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if !elem.AvgRating.Decimal.Equal(avgRating.Decimal) {
-								isValid[4] = false
-							}
-						}
-
-						// genre_id
-						genreID, ok := maps.GenreId[key]
-
-						if !ok {
-							maps.GenreId[key] = elem.GenreId
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.GenreId != genreID {
-								isValid[5] = false
-							}
-						}
-
-						// genre
-						genre, ok := maps.Genre[key]
-
-						if !ok {
-							maps.Genre[key] = elem.Genre
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.Genre.String != genre.String {
-								isValid[6] = false
-							}
-						}
-
-						// member_id
-						memberID, ok := maps.MemberId[key]
-
-						if !ok {
-							maps.MemberId[key] = elem.MemberId
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.MemberId != memberID {
-								isValid[7] = false
-							}
-						}
-
-						// birthYear
-						birthYear, ok := maps.BirthYear[key]
-
-						if !ok {
-							maps.BirthYear[key] = elem.BirthYear
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.BirthYear.Int32 != birthYear.Int32 {
-								isValid[8] = false
-							}
-						}
-
-						// role
-						role, ok := maps.Role[key]
-
-						if !ok {
-							maps.Role[key] = elem.Role
-						} else {
-							// Since they differ, this is not a valid functional dependency
-							if elem.Role != role {
-								isValid[9] = false
-							}
-						}
-					}
-
-					header := ""
-
-					switch groupOne {
-					case 0:
-						header += "movieID,"
-					case 1:
-						header += "type,"
-					case 2:
-						header += "startYear,"
-					case 3:
-						header += "runtimeMinutes,"
-					case 4:
-						header += "avgRating,"
-					case 5:
-						header += "genre_id,"
-					case 6:
-						header += "genre,"
-					case 7:
-						header += "member_id,"
-					case 8:
-						header += "birthYear,"
-					case 9:
-						header += "role,"
-					}
-
-					switch groupTwo {
-					case 0:
-						header += "movieID,"
-					case 1:
-						header += "type,"
-					case 2:
-						header += "startYear,"
-					case 3:
-						header += "runtimeMinutes,"
-					case 4:
-						header += "avgRating,"
-					case 5:
-						header += "genre_id,"
-					case 6:
-						header += "genre,"
-					case 7:
-						header += "member_id,"
-					case 8:
-						header += "birthYear,"
-					case 9:
-						header += "role,"
-					}
-
-					switch groupThree {
-					case 0:
-						header += "movieID->"
-					case 1:
-						header += "type->"
-					case 2:
-						header += "startYear->"
-					case 3:
-						header += "runtimeMinutes->"
-					case 4:
-						header += "avgRating->"
-					case 5:
-						header += "genre_id->"
-					case 6:
-						header += "genre->"
-					case 7:
-						header += "member_id->"
-					case 8:
-						header += "birthYear->"
-					case 9:
-						header += "role->"
-					}
-
-					for idx, valid := range isValid {
-						if valid && idx != groupOne && idx != groupTwo && idx != groupThree {
-							switch idx {
-							case 0:
-								println(header + "movieID")
-							case 1:
-								println(header + "type")
-							case 2:
-								println(header + "startYear")
-							case 3:
-								println(header + "runtimeMinutes")
-							case 4:
-								println(header + "avgRating")
-							case 5:
-								println(header + "genre_id")
-							case 6:
-								println(header + "genre")
-							case 7:
-								println(header + "member_id")
-							case 8:
-								println(header + "birthYear")
-							case 9:
-								println(header + "role")
-							}
-						}
-					}
-				}
-			}
-		}
-	}
-
 	fmt.Println("Check Three Done")
 }
 
@@ -825,42 +819,14 @@ func checkFourOnLeft(wg *sync.WaitGroup, data []movieTitleActorNaive) {
 
 	defer wg.Done()
 
-	numMap := make(map[int]bool)
-
 	// Iterate through each column in db
 	for groupOne := 0; groupOne < 10; groupOne++ {
 
-		numMap[groupOne] = true
-
 		for groupTwo := 0; groupTwo < 10; groupTwo++ {
-
-			_, ok := numMap[groupTwo]
-
-			if !ok {
-				continue
-			}
-
-			numMap[groupTwo] = true
 
 			for groupThree := 0; groupThree < 10; groupThree++ {
 
-				_, ok := numMap[groupThree]
-
-				if !ok {
-					continue
-				}
-
-				numMap[groupThree] = true
-
 				for groupFour := 0; groupFour < 10; groupFour++ {
-
-					_, ok := numMap[groupFour]
-
-					if !ok {
-						continue
-					}
-
-					numMap[groupFour] = true
 
 					// All default to being valid functional dependencies. Change to false once we discover they are not
 					// Reset after each group
