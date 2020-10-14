5adcbf194968333c1b22b77132be0030a94b56cf
dmoore91 <danmoore2706@gmail.com>
2020-10-14 00:53:12 -0400

    Change loadData.go to read from Assignment Two db data with minimum requests

--- Four/loadData.go
+++ Four/loadData.go
@@ -9,7 +9,6 @@ import (
 	log "github.com/sirupsen/logrus"
 	"go.mongodb.org/mongo-driver/mongo"
 	"go.mongodb.org/mongo-driver/mongo/options"
-	"strconv"
 	"sync"
 	"time"
 )
@@ -60,18 +59,24 @@ type person struct {
 type dbPerson struct {
 	MemberID    sql.NullInt32  `json:"_id"`
 	PrimaryName sql.NullString `json:"name"`
-	BirthYear   sql.NullString `json:"birthYear"`
-	DeathYear   sql.NullString `json:"deathYear"`
+	BirthYear   sql.NullInt32  `json:"birthYear"`
+	DeathYear   sql.NullInt32  `json:"deathYear"`
 }
 
-func getGenresForTitle(titleId int, conn *pgx.Conn) []string {
+func getGenresForTitle() map[int][]string {
 
-	queryString := "SELECT Genre.genre " +
+	genreMap := make(map[int][]string)
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "SELECT title, Genre.genre " +
 		"FROM Title_Genre " +
-		"INNER JOIN Genre ON Title_Genre.genre = genre.id " +
-		"WHERE Title_Genre.title=$1;"
+		"INNER JOIN Genre ON Title_Genre.genre = genre.id "
 
-	rows, err := conn.Query(context.Background(), queryString, titleId)
+	rows, err := conn.Query(context.Background(), queryString)
 
 	if err != nil {
 		log.Fatal(err)
@@ -79,30 +84,41 @@ func getGenresForTitle(titleId int, conn *pgx.Conn) []string {
 
 	defer rows.Close()
 
-	var genres []string
-
 	for rows.Next() {
 
+		var titleID int
 		var genre string
 
-		err = rows.Scan(&genre)
+		err = rows.Scan(&titleID, &genre)
 		if err != nil {
 			log.Fatal(err)
 		}
 
-		genres = append(genres, genre)
+		genreMap[titleID] = append(genreMap[titleID], genre)
 	}
 
-	return genres
+	err = conn.Close(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return genreMap
 }
 
-func getDirectorsForTitle(titleId int, conn *pgx.Conn) []int {
+func getDirectorsForTitle() map[int][]int {
+
+	directorsMap := make(map[int][]int)
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
 
 	queryString := "SELECT director " +
-		"FROM Title_Director " +
-		"WHERE title=$1;"
+		"FROM Title_Director "
 
-	rows, err := conn.Query(context.Background(), queryString, titleId)
+	rows, err := conn.Query(context.Background(), queryString)
 
 	if err != nil {
 		log.Fatal(err)
@@ -110,10 +126,9 @@ func getDirectorsForTitle(titleId int, conn *pgx.Conn) []int {
 
 	defer rows.Close()
 
-	var directors []int
-
 	for rows.Next() {
 
+		var titleID int
 		var director int
 
 		err = rows.Scan(&director)
@@ -121,150 +136,215 @@ func getDirectorsForTitle(titleId int, conn *pgx.Conn) []int {
 			log.Fatal(err)
 		}
 
-		directors = append(directors, director)
+		directorsMap[titleID] = append(directorsMap[titleID], director)
 	}
 
-	return directors
-}
-
-func getWritersForTitle(titleId int, conn *pgx.Conn) []int {
-
-	queryString := "SELECT writer " +
-		"FROM Title_Writer " +
-		"WHERE title=$1;"
-
-	rows, err := conn.Query(context.Background(), queryString, titleId)
+	err = conn.Close(context.Background())
 
 	if err != nil {
 		log.Fatal(err)
 	}
 
-	defer rows.Close()
-
-	var writers []int
-
-	for rows.Next() {
-
-		var writer int
-
-		err = rows.Scan(&writer)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		writers = append(writers, writer)
-	}
-
-	return writers
+	return directorsMap
 }
 
-func getProducersForTitle(titleId int, conn *pgx.Conn) []int {
+func getWritersForTitle() map[int][]int {
 
-	queryString := "SELECT producer " +
-		"FROM Title_Producer " +
-		"WHERE title=$1;"
-
-	rows, err := conn.Query(context.Background(), queryString, titleId)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	var producers []int
-
-	for rows.Next() {
-
-		var producer int
-
-		err = rows.Scan(&producer)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		producers = append(producers, producer)
-	}
-
-	return producers
-}
-
-func getRolesForActorForTitle(titleId int, actorID int, conn *pgx.Conn) actor {
-
-	queryString := "SELECT Role.role " +
-		"FROM Actor_Title_Role " +
-		"INNER JOIN Role on Role.id = Actor_Title_Role.role " +
-		"WHERE title=$1 AND actor=$2"
-
-	rows, err := conn.Query(context.Background(), queryString, titleId, actorID)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	var roles []string
-
-	for rows.Next() {
-
-		var role string
-
-		err = rows.Scan(&role)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		roles = append(roles, role)
-	}
-
-	return actor{
-		ActorId: actorID,
-		Roles:   roles,
-	}
-}
-
-func getActorsForTitle(titleId int, conn *pgx.Conn) actorList {
-
-	queryString := "SELECT actor " +
-		"FROM Title_Actor " +
-		"WHERE title=$1;"
-
-	rows, err := conn.Query(context.Background(), queryString, titleId)
-
-	if err != nil {
-		log.Fatal(err)
-	}
-
-	defer rows.Close()
-
-	var a actorList
-
-	for rows.Next() {
-
-		var actorID int
-
-		err = rows.Scan(&actorID)
-		if err != nil {
-			log.Fatal(err)
-		}
-
-		a.Actors = append(a.Actors, getRolesForActorForTitle(titleId, actorID, conn))
-	}
-
-	return a
-}
-
-func populateTitleTable(wg *sync.WaitGroup) {
-
-	defer wg.Done()
-
-	client := ConnectToMongo()
+	writerMap := make(map[int][]int)
 
 	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
 	if err != nil {
 		log.Fatal(err)
 	}
+
+	queryString := "SELECT title, writer " +
+		"FROM Title_Writer "
+
+	rows, err := conn.Query(context.Background(), queryString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var titleID int
+		var writer int
+
+		err = rows.Scan(&titleID, &writer)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		writerMap[titleID] = append(writerMap[titleID], writer)
+	}
+
+	err = conn.Close(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return writerMap
+}
+
+func getProducersForTitle() map[int][]int {
+
+	producerMap := make(map[int][]int)
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "SELECT title, producer " +
+		"FROM Title_Producer "
+
+	rows, err := conn.Query(context.Background(), queryString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var titleID int
+		var producer int
+
+		err = rows.Scan(&titleID, &producer)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		producerMap[titleID] = append(producerMap[titleID], producer)
+	}
+
+	err = conn.Close(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return producerMap
+}
+
+func getRolesForActorForTitle() map[int]map[int][]string {
+
+	rolesMap := make(map[int]map[int][]string)
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "SELECT title, actor, Role.role " +
+		"FROM Actor_Title_Role " +
+		"INNER JOIN Role on Role.id = Actor_Title_Role.role "
+
+	rows, err := conn.Query(context.Background(), queryString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var titleID int
+		var actorID int
+		var role string
+
+		err = rows.Scan(&titleID, &actorID, &role)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		_, ok := rolesMap[titleID][actorID]
+
+		if ok {
+			rolesMap[titleID][actorID] = append(rolesMap[titleID][actorID], role)
+		} else {
+			rolesMap[titleID] = make(map[int][]string)
+			rolesMap[titleID][actorID] = append(rolesMap[titleID][actorID], role)
+		}
+	}
+
+	err = conn.Close(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return rolesMap
+}
+
+func getActorsForTitle() map[int]actorList {
+
+	rolesMap := getRolesForActorForTitle()
+
+	titleActorMap := make(map[int]actorList)
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "SELECT title, actor " +
+		"FROM Title_Actor"
+	rows, err := conn.Query(context.Background(), queryString)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer rows.Close()
+
+	for rows.Next() {
+
+		var actorID int
+		var titleID int
+
+		err = rows.Scan(&titleID, &actorID)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		a := actor{
+			ActorId: actorID,
+			Roles:   rolesMap[titleID][actorID],
+		}
+
+		l := titleActorMap[titleID].Actors
+
+		l = append(l, a)
+
+		titleActorMap[titleID] = actorList{Actors: l}
+	}
+
+	err = conn.Close(context.Background())
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return titleActorMap
+}
+
+func readTitleTable() []title {
+
+	var titleList []title
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignmenttwo")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer conn.Close(context.Background())
 
 	queryString := "SELECT id, type, originalTitle, startYear, endYear, runtimeminutes, avgrating, numvotes " +
 		"FROM Title "
@@ -321,21 +401,44 @@ func populateTitleTable(wg *sync.WaitGroup) {
 			t.NumVotes = int(db.NumVotes.Int32)
 		}
 
-		t.Genres = getGenresForTitle(t.Id, conn)
-		t.Directors = getDirectorsForTitle(t.Id, conn)
-		t.Writers = getWritersForTitle(t.Id, conn)
-		t.Producers = getProducersForTitle(t.Id, conn)
-		t.Actors = getActorsForTitle(t.Id, conn)
-
-		_, err = client.Database("assignment_four").Collection("Movies").InsertOne(context.Background(), t)
-		if err != nil {
-			log.Fatal(err)
-		}
+		titleList = append(titleList, t)
 	}
 
-	err = conn.Close(context.Background())
-	if err != nil {
-		log.Fatal(err)
+	return titleList
+}
+
+func addMovies(wg *sync.WaitGroup) {
+
+	defer wg.Done()
+
+	titles := readTitleTable()
+	fmt.Println("Got Titles")
+
+	producerMap := getProducersForTitle()
+	fmt.Println("Got Producers")
+
+	writerMap := getWritersForTitle()
+	fmt.Println("Got Writers")
+
+	directorMap := getDirectorsForTitle()
+	fmt.Println("Got Directors")
+
+	genreMap := getGenresForTitle()
+	fmt.Println("Got Genres")
+
+	titleActorMap := getActorsForTitle()
+	fmt.Println("Got Actors")
+
+	client := ConnectToMongo()
+
+	for _, title := range titles {
+		title.Genres = genreMap[title.Id]
+		title.Writers = writerMap[title.Id]
+		title.Producers = producerMap[title.Id]
+		title.Actors = titleActorMap[title.Id]
+		title.Directors = directorMap[title.Id]
+
+		client.Database("assignment_four").Collection("Movies").InsertOne(context.Background(), title)
 	}
 }
 
@@ -365,7 +468,7 @@ func getNamesMap(wg *sync.WaitGroup) {
 
 		var db dbPerson
 
-		err = rows.Scan(&db.MemberID, &db.PrimaryName, db.BirthYear, db.DeathYear)
+		err = rows.Scan(&db.MemberID, &db.PrimaryName, &db.BirthYear, &db.DeathYear)
 		if err != nil {
 			log.Fatal(err)
 		}
@@ -381,26 +484,26 @@ func getNamesMap(wg *sync.WaitGroup) {
 		}
 
 		if db.BirthYear.Valid {
-			p.BirthYear, err = strconv.Atoi(db.BirthYear.String)
-			if err != nil {
-				log.Fatal(err)
-			}
+			p.BirthYear = int(db.BirthYear.Int32)
 		}
 
 		if db.DeathYear.Valid {
-			p.DeathYear, err = strconv.Atoi(db.DeathYear.String)
-			if err != nil {
-				log.Fatal(err)
-			}
+			p.DeathYear = int(db.DeathYear.Int32)
 		}
 
-		client.Database("assignment_four").Collection("Members").InsertOne(context.Background(), p)
+		_, err = client.Database("assignment_four").Collection("Members").InsertOne(context.Background(), p)
+
+		if err != nil {
+			log.Fatal(err)
+		}
 	}
 
 	err = conn.Close(context.Background())
+
 	if err != nil {
 		log.Fatal(err)
 	}
+
 }
 
 func ConnectToMongo() *mongo.Client {
@@ -431,7 +534,7 @@ func main() {
 
 	wg.Add(2)
 
-	go populateTitleTable(wg)
+	go addMovies(wg)
 	go getNamesMap(wg)
 
 	wg.Wait()
