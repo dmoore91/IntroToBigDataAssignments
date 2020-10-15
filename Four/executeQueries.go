4a899c9833f0745392686bccef2ca0991eb67b58
dmoore91 <danmoore2706@gmail.com>
2020-10-15 02:08:30 -0400

    Get Sci-Fi movies with James Cameron and Sigourney Weaver

--- Four/executeQueries.go
+++ Four/executeQueries.go
@@ -107,7 +107,49 @@ func avgRuntimeWrittenByLivingBhardwaj() {
 	fmt.Println("It took  " + elapsed.String() + " to run this query")
 }
 
+func getSciFiMovies() {
+
+	client := connectToMongoQuery()
+
+	start := time.Now()
+
+	unwindActorsStage := bson.D{{"$unwind", "$actors.actors"}}
+	unwindGenresStage := bson.D{{"$unwind", "$genres"}}
+	unwindDirectorsStage := bson.D{{"$unwind", "$directors"}}
+	joinWithMembersStageActor := bson.D{{"$lookup", bson.D{{"from", "Members"},
+		{"localField", "actors.actors.actor"}, {"foreignField", "_id"}, {"as", "actor"}}}}
+	joinWithMembersStageDirector := bson.D{{"$lookup", bson.D{{"from", "Members"},
+		{"localField", "directors"}, {"foreignField", "_id"}, {"as", "director"}}}}
+	getSciFi := bson.D{{"$match", bson.D{{"genres", "Sci-Fi"}}}}
+	getJamesCameronStage := bson.D{{"$match", bson.D{{"director.name", "James Cameron"}}}}
+	getSigourneyWeaverStage := bson.D{{"$match", bson.D{{"actor._id", 244}}}}
+
+	showInfoCursor, err := client.Database("assignment_four").Collection("Movies").Aggregate(context.Background(),
+		mongo.Pipeline{unwindActorsStage, unwindGenresStage, unwindDirectorsStage, joinWithMembersStageActor,
+			joinWithMembersStageDirector, getSciFi, getJamesCameronStage, getSigourneyWeaverStage})
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	for showInfoCursor.Next(context.Background()) {
+		fmt.Println(showInfoCursor.Current)
+	}
+
+	err = showInfoCursor.Close(context.Background())
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	t := time.Now()
+	elapsed := t.Sub(start)
+	fmt.Println("It took  " + elapsed.String() + " to run this query")
+
+}
+
 func main() {
 	//actorsNamedPhiAndDidntActIn2014()
-	avgRuntimeWrittenByLivingBhardwaj()
+	//avgRuntimeWrittenByLivingBhardwaj()
+	getSciFiMovies()
 }
