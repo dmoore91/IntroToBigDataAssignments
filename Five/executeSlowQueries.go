e52a80aa98fb4195f9084437657da1a4f3671ed0
dmoore91 <danmoore2706@gmail.com>
2020-10-20 00:53:07 -0400

    Make notFunny more efficient

--- Five/executeSlowQueries.go
+++ Five/executeSlowQueries.go
@@ -85,13 +85,15 @@ func notFunnyActorsNM(wg *sync.WaitGroup) {
 		log.Fatal(err)
 	}
 
-	queryString := "SELECT name " +
+	queryString := "(SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActor) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActor)) as All_Actor " +
-		"WHERE name LIKE 'Ja%' AND id NOT IN (SELECT All_Actor.id " +
+		"WHERE name LIKE 'Ja%') " +
+		"EXCEPT " +
+		"(SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActor) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActor)) as All_Actor " +
 		"INNER JOIN (SELECT actor, title FROM ActedIn) as All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id " +
 		"INNER JOIN ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovie) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovie)) AS All_Movie ON All_Movie.id = All_Movie_Actor.title " +
-		"WHERE All_Movie.genre LIKE 'Comedy')"
+		"WHERE name LIKE 'Ja%' and All_Movie.genre LIKE 'Comedy')"
 
 	_, err = conn.Exec(context.Background(), queryString)
 
@@ -119,14 +121,15 @@ func notFunnyActorsM(wg *sync.WaitGroup) {
 		log.Fatal(err)
 	}
 
-	queryString := "SELECT name " +
+	queryString := "(SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActorMaterialized) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActorMaterialized)) as All_Actor " +
-		"WHERE name LIKE 'Ja%' AND id NOT IN (SELECT All_Actor.id " +
+		"WHERE name LIKE 'Ja%') " +
+		"EXCEPT " +
+		"(SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActorMaterialized) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActorMaterialized)) as All_Actor " +
-		"INNER JOIN (SELECT actor, title FROM ActedInMaterialized) as All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id " +
-		"INNER JOIN ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie " +
-		"ON All_Movie.id = All_Movie_Actor.title " +
-		"WHERE All_Movie.genre LIKE 'Comedy')"
+		"INNER JOIN (SELECT actor, title FROM ActedIn) as All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id " +
+		"INNER JOIN ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie ON All_Movie.id = All_Movie_Actor.title " +
+		"WHERE name LIKE 'Ja%' and All_Movie.genre LIKE 'Comedy')"
 
 	_, err = conn.Exec(context.Background(), queryString)
 
