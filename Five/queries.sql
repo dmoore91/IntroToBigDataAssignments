482d29588f30e25a52708fa24bfd1e41aab21cc4
dmoore91 <danmoore2706@gmail.com>
2020-10-20 00:47:00 -0400

    Fix up not funny actor query

--- Five/queries.sql
+++ Five/queries.sql
@@ -5,10 +5,9 @@ WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id)
                 INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
                 WHERE startYear BETWEEN 2000 AND 2005 AND All_Movie_Actor.actor = actor.id) > 10;
 
-SELECT name
-FROM All_Actor AS actor
-WHERE name LIKE 'Ja%' AND id NOT IN (SELECT id
-                                     FROM All_Actor
-                                     INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
-                                     INNER JOIN All_Movie ON All_Movie.id = All_Movie_Actor.title
-                                     WHERE All_Movie.genre LIKE 'Comedy')
+(SELECT name FROM All_Actor AS actor WHERE name LIKE 'Ja%')
+EXCEPT
+(SELECT name FROM All_Actor AS actor
+    INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
+    INNER JOIN All_Movie ON All_Movie.id = All_Movie_Actor.title
+    WHERE name LIKE 'Ja%' AND All_Movie.genre LIKE 'Comedy')
