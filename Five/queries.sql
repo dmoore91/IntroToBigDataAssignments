8bcbdc5db130e482f53a67dbd7dcf7141e3f465b
dmoore91 <danmoore2706@gmail.com>
2020-10-19 21:35:12 -0400

    Remove unnecessary join

--- Five/queries.sql
+++ Five/queries.sql
@@ -6,9 +6,6 @@ WHERE (SELECT COUNT(All_Movie.id)
                 WHERE startYear BETWEEN 2000 AND 2005 AND All_Movie_Actor.actor = actor.id) > 10;
 
 SELECT name
-FROM All_Actor
-INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
-INNER JOIN All_Movie ON All_Movie.id = All_Movie_Actor.title
 WHERE name LIKE 'Ja%' AND id NOT IN (SELECT id
                                      FROM All_Actor
                                      INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
