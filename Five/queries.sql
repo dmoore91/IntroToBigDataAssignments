2b7a3d5d14e4aef2f275e6e9471badc5ff68a5ee
dmoore91 <danmoore2706@gmail.com>
2020-10-19 21:25:27 -0400

    Write query for actors that aren't funny

--- Five/queries.sql
+++ Five/queries.sql
@@ -3,4 +3,14 @@ FROM All_Actor AS actor
 WHERE (SELECT COUNT(All_Movie.id)
                 FROM All_Movie
                 INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
-                WHERE startYear BETWEEN 2000 AND 2005 and All_Movie_Actor.actor = actor.id) > 10;
+                WHERE startYear BETWEEN 2000 AND 2005 AND All_Movie_Actor.actor = actor.id) > 10;
+
+SELECT name
+FROM All_Actor
+INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
+INNER JOIN All_Movie ON All_Movie.id = All_Movie_Actor.title
+WHERE name LIKE 'Ja%' AND id NOT IN (SELECT id
+                                     FROM All_Actor
+                                     INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = All_Actor.id
+                                     INNER JOIN All_Movie ON All_Movie.id = All_Movie_Actor.title
+                                     WHERE All_Movie.genre LIKE 'Comedy')
