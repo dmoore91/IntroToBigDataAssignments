776fb02434cd997b035b760e63158627a2d4d0ce
dmoore91 <danmoore2706@gmail.com>
2020-10-20 07:19:15 -0400

    Make actors query more efficient

--- Five/executeSlowQueries.go
+++ Five/executeSlowQueries.go
@@ -19,12 +19,14 @@ func productiveActorsNM(wg *sync.WaitGroup) {
 		log.Fatal(err)
 	}
 
-	queryString := "SELECT name " +
+	queryString := "SELECT actors.name " +
+		"FROM (SELECT name, COUNT(actor.id) " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActor) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActor)) AS actor " +
-		"WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id) " +
-		"FROM ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovie) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovie)) AS All_Movie " +
-		"INNER JOIN (SELECT actor, title FROM ActedIn) as All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id " +
-		"WHERE startYear BETWEEN 2000 AND 2005 and All_Movie_Actor.actor = actor.id) > 10;"
+		"INNER JOIN (SELECT actor, title FROM ActedIn) as All_Movie_Actor ON All_Movie_Actor.actor = actor.id " +
+		"INNER JOIN ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovie) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovie)) AS All_Movie ON All_Movie_Actor.title = All_Movie.id " +
+		"WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005 " +
+		"GROUP BY name " +
+		"HAVING COUNT(actor.id) > 10) as actors;"
 
 	_, err = conn.Exec(context.Background(), queryString)
 
@@ -52,12 +54,14 @@ func productiveActorsM(wg *sync.WaitGroup) {
 		log.Fatal(err)
 	}
 
-	queryString := "SELECT name " +
+	queryString := "SELECT actors.name " +
+		"FROM (SELECT name, COUNT(actor.id) " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActorMaterialized) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActorMaterialized)) AS actor " +
-		"WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id) " +
-		"FROM ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie " +
-		"INNER JOIN (SELECT actor, title FROM ActedInMaterialized) as All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id " +
-		"WHERE startYear BETWEEN 2000 AND 2005 and All_Movie_Actor.actor = actor.id) > 10;"
+		"INNER JOIN (SELECT actor, title FROM ActedInMaterialized) as All_Movie_Actor ON All_Movie_Actor.actor = actor.id " +
+		"INNER JOIN ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie ON All_Movie_Actor.title = All_Movie.id " +
+		"WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005 " +
+		"GROUP BY name " +
+		"HAVING COUNT(actor.id) > 10) as actors;"
 
 	_, err = conn.Exec(context.Background(), queryString)
 
--- Five/queries.sql
+++ Five/queries.sql
@@ -1,9 +1,11 @@
-SELECT name
+SELECT actors.name
+FROM (SELECT name, COUNT(actor.id)
 FROM All_Actor AS actor
-WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id)
-                FROM All_Movie
-                INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
-                WHERE startYear BETWEEN 2000 AND 2005 AND All_Movie_Actor.actor = actor.id) > 10;
+INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = actor.id
+INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
+WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
+GROUP BY name
+HAVING COUNT(actor.id) > 10) as actors;
 
 (SELECT name FROM All_Actor AS actor WHERE name LIKE 'Ja%')
 EXCEPT
