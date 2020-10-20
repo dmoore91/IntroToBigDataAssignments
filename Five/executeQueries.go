3a4a2e9784ebc203bcd5e471575f1c3f2c506d47
dmoore91 <danmoore2706@gmail.com>
2020-10-19 23:28:41 -0400

    Don't look at dead actors

--- Five/executeQueries.go
+++ Five/executeQueries.go
@@ -21,7 +21,7 @@ func productiveActorsNM(wg *sync.WaitGroup) {
 
 	queryString := "SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActor) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActor)) AS actor " +
-		"WHERE (SELECT COUNT(All_Movie.id) " +
+		"WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id) " +
 		"FROM ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovie) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovie)) AS All_Movie " +
 		"INNER JOIN (SELECT actor, title FROM ActedIn) as All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id " +
 		"WHERE startYear BETWEEN 2000 AND 2005 and All_Movie_Actor.actor = actor.id) > 10;"
@@ -55,7 +55,7 @@ func productiveActorsM(wg *sync.WaitGroup) {
 	queryString := "SELECT name " +
 		"FROM ((SELECT id, name, birthYear, deathYear FROM ComedyActorMaterialized) UNION (SELECT id, name, birthYear, deathYear FROM NonComedyActorMaterialized)) AS actor " +
 		"WHERE (SELECT COUNT(All_Movie.id) " +
-		"FROM ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie " +
+		"FROM actor.deathYear IS NULL AND ((SELECT id, title, startYear, 'Comedy' AS genre FROM ComedyMovieMaterialized) UNION (SELECT id, title, startYear, 'Not Comedy' AS genre FROM NonComedyMovieMaterialized)) AS All_Movie " +
 		"INNER JOIN (SELECT actor, title FROM ActedInMaterialized) as All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id " +
 		"WHERE startYear BETWEEN 2000 AND 2005 and All_Movie_Actor.actor = actor.id) > 10;"
 
--- Five/queries.sql
+++ Five/queries.sql
@@ -1,6 +1,6 @@
 SELECT name
 FROM All_Actor AS actor
-WHERE (SELECT COUNT(All_Movie.id)
+WHERE actor.deathYear IS NULL AND (SELECT COUNT(All_Movie.id)
                 FROM All_Movie
                 INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
                 WHERE startYear BETWEEN 2000 AND 2005 AND All_Movie_Actor.actor = actor.id) > 10;
