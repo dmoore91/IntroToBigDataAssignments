54bc814bbae41da73c74a82e977f281dd703b03b
dmoore91 <danmoore2706@gmail.com>
2020-10-20 20:14:03 -0400

    Fix query

--- Five/smartQueries.sql
+++ Five/smartQueries.sql
@@ -1,20 +1,20 @@
-(SELECT actors.name
+SELECT actors.name
 FROM (SELECT name, COUNT(actor.id)
-FROM ComedyActor AS actor
-INNER JOIN ActedIn ON ActedIn.actor = actor.id
-INNER JOIN ComedyMovie ON ComedyMovie.title = ComedyMovie.id
-WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
-GROUP BY name
-HAVING COUNT(actor.id) > 10) as actors;)
-UNION
-(SELECT actors.name
-FROM (SELECT name, COUNT(actor.id)
-FROM NonComedyActor AS actor
-INNER JOIN ActedIn ON ActedIn.actor = actor.id
-INNER JOIN NonComedyMovie ON NonComedyMovie.title = NonComedyMovie.id
-WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
-GROUP BY name
-HAVING COUNT(actor.id) > 10) as actors;)
+        FROM ComedyActor AS actor
+        INNER JOIN ActedIn ON ActedIn.actor = actor.id
+        INNER JOIN ComedyMovie ON ActedIn.title = ComedyMovie.id
+        WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
+        GROUP BY name
+        HAVING COUNT(actor.id) > 10
+     UNION
+     SELECT name, COUNT(actor.id)
+        FROM NonComedyActor AS actor
+        INNER JOIN ActedIn ON ActedIn.actor = actor.id
+        INNER JOIN NonComedyMovie ON ActedIn.title = NonComedyMovie.id
+        WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
+        GROUP BY name
+        HAVING COUNT(actor.id) > 10) as actors
+
 
 
 SELECT name
