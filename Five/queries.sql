b040dc4a4a738f310f463b03f4286f5a30a67c54
dmoore91 <danmoore2706@gmail.com>
2020-10-20 20:13:48 -0400

    Clean up query

--- Five/queries.sql
+++ Five/queries.sql
@@ -2,7 +2,7 @@ SELECT actors.name
 FROM (SELECT name, COUNT(actor.id)
 FROM All_Actor AS actor
 INNER JOIN All_Movie_Actor ON All_Movie_Actor.actor = actor.id
-INNER JOIN All_Movie_Actor ON All_Movie_Actor.title = All_Movie.id
+INNER JOIN All_Movie ON All_Movie_Actor.title = All_Movie.id
 WHERE actor.deathYear IS NULL AND  startYear BETWEEN 2000 AND 2005
 GROUP BY name
 HAVING COUNT(actor.id) > 10) as actors;
