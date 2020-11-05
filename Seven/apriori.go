8dcc973f9f9fe64e03f20a2282e006c3796fb308
dmoore91 <danmoore2706@gmail.com>
2020-11-05 02:47:34 -0500

    Most recent attempt at creating L2. Cartesian Product part works great but it seems like thr grouping/counting is failing somehow

--- Seven/apriori.go
+++ Seven/apriori.go
@@ -37,13 +37,41 @@ func createL1() {
 	}
 }
 
+//TODO L2 doesn't currently satisfy requirements
+func createL2() {
+
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignment_seven")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	queryString := "CREATE TABLE L2 AS " +
+		"(SELECT L1.actor as actor1, tmp.actor as actor2, COUNT(*) as count FROM L1 CROSS JOIN L1 as tmp GROUP BY L1.actor, tmp.actor) "
+
+	commandTag, err := conn.Exec(context.Background(), queryString)
+
+	if err != nil {
+		log.Error(err)
+	}
+
+	if commandTag.RowsAffected() == 0 {
+		log.Error(err)
+	}
+
+	err = conn.Close(context.Background())
+	if err != nil {
+		log.Error(err)
+	}
+}
+
 // Minimum support is 5
 // Therefore we must only keep entries with a count >=5 for
 // all tables
 func main() {
 	start := time.Now()
 
-	createL1()
+	//createL1()
+	createL2()
 
 	t := time.Now()
 	elapsed := t.Sub(start)
