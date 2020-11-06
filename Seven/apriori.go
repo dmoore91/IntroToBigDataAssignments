b1290c96e471bc682c99ceaec04633265d5ddc16
dmoore91 <danmoore2706@gmail.com>
2020-11-05 22:38:12 -0500

    Correct and performant code for L2

--- Seven/apriori.go
+++ Seven/apriori.go
@@ -37,7 +37,6 @@ func createL1() {
 	}
 }
 
-//TODO L2 doesn't currently satisfy requirements
 func createL2() {
 
 	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignment_seven")
@@ -45,8 +44,15 @@ func createL2() {
 		log.Fatal(err)
 	}
 
-	queryString := "CREATE TABLE L2 AS " +
-		"(SELECT L1.actor as actor1, tmp.actor as actor2, COUNT(*) as count FROM L1 CROSS JOIN L1 as tmp GROUP BY L1.actor, tmp.actor) "
+	queryString :=
+		"CREATE TABLE L2 AS " +
+			"(SELECT L1.actor as actor1, tmp.actor as actor2, COUNT(*) " +
+			"FROM L1 CROSS JOIN L1 as tmp " +
+			"INNER JOIN (SELECT a.actor as actorA, b.actor as actorB FROM Popular_Movie_Actors as a, Popular_Movie_Actors as b WHERE a.title = b.title AND a.actor != b.actor) as a ON " +
+			"(a.actorA = L1.actor AND a.actorB = tmp.actor) " +
+			"WHERE L1.actor < tmp.actor " +
+			"GROUP BY L1.actor, tmp.actor " +
+			"HAVING COUNT(*) >= 5)"
 
 	commandTag, err := conn.Exec(context.Background(), queryString)
 
