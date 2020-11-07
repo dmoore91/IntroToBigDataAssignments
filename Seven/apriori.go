fbcb708cb8ae2ab48818ee84b172f40671334cf7
dmoore91 <danmoore2706@gmail.com>
2020-11-06 19:32:32 -0500

    Working lattice generation code

--- Seven/apriori.go
+++ Seven/apriori.go
@@ -5,6 +5,7 @@ import (
 	"fmt"
 	"github.com/jackc/pgx"
 	log "github.com/sirupsen/logrus"
+	"strconv"
 	"time"
 )
 
@@ -100,6 +101,199 @@ func createL3() {
 	}
 }
 
+func generateSelectString(lattice int) string {
+
+	selectString := "SELECT "
+
+	for i := 1; i < lattice; i++ {
+		selectString += "p.actor"
+		selectString += strconv.Itoa(i)
+		selectString += " as actor"
+		selectString += strconv.Itoa(i)
+		selectString += ", "
+	}
+
+	selectString += "q.actor"
+	selectString += strconv.Itoa(lattice - 1)
+	selectString += " as actor"
+	selectString += strconv.Itoa(lattice)
+	selectString += ", COUNT(*) as count "
+
+	return selectString
+}
+
+func generateFromStatement(lattice int) string {
+
+	alphabetMap := make(map[int]string)
+	alphabetMap[0] = "a"
+	alphabetMap[1] = "b"
+	alphabetMap[2] = "c"
+	alphabetMap[3] = "d"
+	alphabetMap[4] = "e"
+	alphabetMap[5] = "f"
+	alphabetMap[6] = "g"
+	alphabetMap[7] = "h"
+	alphabetMap[8] = "i"
+	alphabetMap[9] = "j"
+	alphabetMap[10] = "k"
+	alphabetMap[11] = "l"
+	alphabetMap[12] = "m"
+	alphabetMap[13] = "n"
+	alphabetMap[14] = "o"
+	alphabetMap[15] = "r"
+	alphabetMap[16] = "s"
+	alphabetMap[17] = "t"
+	alphabetMap[18] = "u"
+	alphabetMap[19] = "v"
+	alphabetMap[20] = "w"
+	alphabetMap[21] = "x"
+	alphabetMap[22] = "y"
+	alphabetMap[23] = "z"
+
+	fromString := "FROM "
+	fromString += "L"
+	fromString += strconv.Itoa(lattice - 1)
+	fromString += " p, "
+	fromString += "L"
+	fromString += strconv.Itoa(lattice - 1)
+	fromString += " q"
+
+	for i := 0; i < lattice; i++ {
+		fromString += ", "
+		fromString += "Popular_Movie_Actors "
+		fromString += alphabetMap[i]
+	}
+	fromString += " "
+
+	return fromString
+}
+
+func generateWhereString(lattice int) string {
+
+	whereString := "WHERE "
+
+	for i := 1; i <= lattice-2; i++ {
+		whereString += "p.actor"
+		whereString += strconv.Itoa(i)
+		whereString += " = "
+		whereString += "q.actor"
+		whereString += strconv.Itoa(i)
+		whereString += " AND "
+	}
+
+	whereString += "p.actor"
+	whereString += strconv.Itoa(lattice - 1)
+	whereString += " < "
+	whereString += "q.actor"
+	whereString += strconv.Itoa(lattice - 1)
+
+	alphabetMap := make(map[int]string)
+	alphabetMap[0] = "a"
+	alphabetMap[1] = "b"
+	alphabetMap[2] = "c"
+	alphabetMap[3] = "d"
+	alphabetMap[4] = "e"
+	alphabetMap[5] = "f"
+	alphabetMap[6] = "g"
+	alphabetMap[7] = "h"
+	alphabetMap[8] = "i"
+	alphabetMap[9] = "j"
+	alphabetMap[10] = "k"
+	alphabetMap[11] = "l"
+	alphabetMap[12] = "m"
+	alphabetMap[13] = "n"
+	alphabetMap[14] = "o"
+	alphabetMap[15] = "r"
+	alphabetMap[16] = "s"
+	alphabetMap[17] = "t"
+	alphabetMap[18] = "u"
+	alphabetMap[19] = "v"
+	alphabetMap[20] = "w"
+	alphabetMap[21] = "x"
+	alphabetMap[22] = "y"
+	alphabetMap[23] = "z"
+
+	for i := 1; i < lattice; i++ {
+		whereString += " AND "
+		whereString += alphabetMap[i-1]
+		whereString += ".actor = p.actor"
+		whereString += strconv.Itoa(i)
+	}
+
+	whereString += " AND "
+	whereString += alphabetMap[lattice-1]
+	whereString += ".actor = q.actor"
+	whereString += strconv.Itoa(lattice - 1)
+
+	for i := 0; i < lattice-1; i++ {
+		whereString += " AND "
+		whereString += alphabetMap[i]
+		whereString += ".title = "
+		whereString += alphabetMap[i+1]
+		whereString += ".title "
+	}
+
+	return whereString
+}
+
+func generateGroupBy(lattice int) string {
+
+	groupByString := "GROUP BY "
+
+	for i := 1; i < lattice; i++ {
+		groupByString += "p.actor"
+		groupByString += strconv.Itoa(i)
+		groupByString += ", "
+	}
+
+	groupByString += "q.actor"
+	groupByString += strconv.Itoa(lattice - 1)
+	groupByString += " "
+
+	return groupByString
+}
+
+func generateAprioriLattices() {
+	conn, err := pgx.Connect(context.Background(), "postgres://postgres@localhost:5432/assignment_seven")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	lattice := 2
+
+	for {
+
+		queryString := "CREATE TABLE L" + strconv.Itoa(lattice) + " AS ("
+		queryString += generateSelectString(lattice)
+		queryString += generateFromStatement(lattice)
+		queryString += generateWhereString(lattice)
+		queryString += generateGroupBy(lattice)
+		queryString += "HAVING COUNT(*) >= 5)"
+
+		fmt.Println(queryString)
+
+		commandTag, err := conn.Exec(context.Background(), queryString)
+
+		if err != nil {
+			log.Error(err)
+		}
+
+		if commandTag.RowsAffected() == 0 {
+			break
+		} else {
+			fmt.Println("Lattice: " + strconv.Itoa(lattice) + " has " + strconv.Itoa(int(commandTag.RowsAffected())) + " entries")
+		}
+
+		lattice += 1
+	}
+
+	err = conn.Close(context.Background())
+	if err != nil {
+		log.Error(err)
+	}
+
+}
+
 // Minimum support is 5
 // Therefore we must only keep entries with a count >=5 for
 // all tables
@@ -108,7 +302,9 @@ func main() {
 
 	//createL1()
 	//createL2()
-	createL3()
+	//createL3()
+
+	generateAprioriLattices()
 
 	t := time.Now()
 	elapsed := t.Sub(start)
