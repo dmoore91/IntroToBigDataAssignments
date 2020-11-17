9a81684a3e4ad0f506da9f2a43da3f9875920e27
dmoore91 <danmoore2706@gmail.com>
2020-11-17 18:35:41 -0500

    Phis query

---
+++ Nine/queries.py
@@ -0,0 +1,35 @@
+from pyspark.sql import SparkSession
+import time
+
+
+def phi_actors():
+
+    start = time.time()
+
+    spark = SparkSession.builder.master("local").appName("Phi").getOrCreate()
+
+    titles = spark.read.csv("title.basics.tsv", sep="\t", header=True)
+    actors = spark.read.csv("name.basics.tsv", sep="\t", nullValue="\\N", header=True)
+    principals = spark.read.csv("title.principals.tsv", sep="\t", header=True)
+
+    title_to_actor = titles.join(principals, on=['tconst'], how='inner')
+    title_to_actor = title_to_actor.join(actors, on=['nconst'], how='inner')
+
+    phis = title_to_actor.filter(title_to_actor.primaryName.startswith("Phi"))
+    living_phis = phis.filter(phis.deathYear.isNull())
+
+    acted_in_2014 = living_phis.filter(living_phis.startYear == 2014)
+
+    didntactin2014 = living_phis.exceptAll(acted_in_2014)
+
+    names = didntactin2014[['primaryName']]
+    unique_names = names.dropDuplicates()
+
+    print("Took: " + str(time.time() - start) + " seconds")
+
+    for row in unique_names.head(10):
+        print(row)
+
+
+if __name__ == '__main__':
+    phi_actors()
